cd2926143ef21b6310e934ad97309b7142851237
rohit6261 <rohitsnjob@gmail.com>
2022-08-28 22:34:55 +0800

    Added code

--- demo.py
+++ demo.py
@@ -1,6 +1,5 @@
 from flask import Flask,request,jsonify, make_response
 import datetime
-import request
 import logging
 import os
 
@@ -8,22 +7,26 @@ import os
 #set up flask parameters
 app = Flask(__name__)
 
-
+current_working_dir = os.getcwd()
 # Set up Logging
 
-log_name=os.path.join(current_working_directory,'api_log.log')
+log_name=os.path.join(current_working_dir,'api_log.log')
 logging.basicConfig(filename=log_name, level=logging.DEBUG,
                    format='%(asctime)s %(message)s',
-                   dateformat='%d%m%Y %H:%M%S')
+                   datefmt='%d%m%Y %H:%M:%S')
 
 @app.route('/', method=['GET'])
 def demo():
+    print(request)
+    print(request.headers)
     if request.headers['Content-Type'] != 'application/json':
-        resp = Response('Hello, World');
+        resp = jsonify('Hello, World')
+       #resp = jsonify({'message': 'Hello, World'})
         return resp
     if request.headers['Content-Type'] == 'application/json':
         return jsonify({'message': 'Hello, World'})
 
 if __name__== '  main  ':
     logging.info('==========Script started======')
-    app.run(host='localhost', port=80, debug=True)
+   #app.run(host='localhost', port=80, debug=True)
+    app.run()
---
+++ readme.md.txt
@@ -0,0 +1,5 @@
+1- ) Install Python
+2-) pip install flask
+3- ) pip install request
+4-) Run demo.py program 
+   #python demo.py
